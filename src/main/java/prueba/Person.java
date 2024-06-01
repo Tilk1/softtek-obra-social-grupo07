cfc0a6bd902129224c4eb9d3cc12057d8effdfa9
Sun <santiagolizondo@gmail.com>
2024-06-01 18:45:28 -0300

    primer commit esqueleto quarkus

---
+++ src/main/java/prueba/Person.java
@@ -0,0 +1,11 @@
+package prueba;
+
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Person extends PanacheEntity {
+    public String name;
+    public LocalDate birth;
+}
