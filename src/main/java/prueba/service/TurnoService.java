90fdc239e66bbd639df70e64c1bb54cc78c94b0a
Dario Emanuel Diaz <dariodiaz91498@gmail.com>
2024-06-06 02:40:53 -0300

    creando endpoin deleteTurno

--- src/main/java/prueba/service/TurnoService.java
+++ src/main/java/prueba/service/TurnoService.java
@@ -6,28 +6,30 @@ import jakarta.enterprise.context.ApplicationScoped;
 import prueba.DTO.TurnoDTO;
 import prueba.model.Turno;
 import prueba.repository.TurnoRepository;
+import lombok.Data;
 
 @ApplicationScoped
+@Data
 public class TurnoService {
 
     @Inject
     TurnoRepository turnoRepository;
 
     public List<Turno> findAllOrderedById() {
-        return this.turnoRepository.findAllOrderedById();
+        return turnoRepository.findAllOrderedById();
     }
 
     public Turno findById(Long id) {
-        return this.turnoRepository.findById(id);
+        return turnoRepository.findById(id);
     }
 
     public void guardarTurno(TurnoDTO turnoDTO) {
-        Turno turno = new Turno(turnoDTO.nombrePaciente, turnoDTO.dniPaciente, turnoDTO.idMedicoEspecialista,
-                turnoDTO.motivoConsulta, turnoDTO.fechaHoraCita);
-        this.turnoRepository.persist(turno);
+        Turno turno = new Turno(turnoDTO.getNombrePaciente(), turnoDTO.getDniPaciente(), turnoDTO.getIdMedicoEspecialista(),
+                turnoDTO.getMotivoConsulta(), turnoDTO.getFechaHoraCita());
+        turnoRepository.persist(turno);
     }
 
     public boolean cancelarTurno(Long id) {
-        return this.turnoRepository.deleteById(id);
+        return turnoRepository.deleteById(id);
     }
 }
