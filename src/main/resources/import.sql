a88ccd394f027c79e06c16a97474b9ac7ed9666f
Dario Emanuel Diaz <dariodiaz91498@gmail.com>
2024-06-08 23:48:05 -0300

    agregando get turnos/{id}

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,15 @@
 -- ESPECIALISTAS
 
-INSERT INTO especialista (id, name, especialidad, horariosConsulta, ubicacion) 
+INSERT INTO especialista (id, nombre, especialidad, horariosConsulta, ubicacion) 
 VALUES (nextval('especialista_seq'), 'Dra. Ana Lopez', 'Dermatologia', 'Lunes a Viernes de 8:00 a 12:00', 'Hospital de Clinicas');
 
-INSERT INTO especialista (id, name, especialidad, horariosConsulta, ubicacion) 
+INSERT INTO especialista (id, nombre, especialidad, horariosConsulta, ubicacion) 
 VALUES (nextval('especialista_seq'), 'Dr. Juan Perez', 'Cardiologia', 'Lunes a Viernes de 8:00 a 12:00', 'Hospital de Clinicas');
 
-INSERT INTO especialista (id, name, especialidad, horariosConsulta, ubicacion)
+INSERT INTO especialista (id, nombre, especialidad, horariosConsulta, ubicacion)
 VALUES (nextval('especialista_seq'), 'Dra. Maria Rodriguez', 'Pediatria', 'Lunes a Viernes de 8:00 a 12:00', 'Hospital de Clinicas');
 
-INSERT INTO especialista (id, name, especialidad, horariosConsulta, ubicacion)
+INSERT INTO especialista (id, nombre, especialidad, horariosConsulta, ubicacion)
 VALUES (nextval('especialista_seq'), 'Dr. Carlos Sanchez', 'Oftalmologia', 'Lunes a Viernes de 8:00 a 12:00', 'Hospital de Clinicas');
 
 -- TURNOS
